7fcd4c5c20c288250303a2ff2da139321c5cec45
Eric Trombly <etrombly@yahoo.com>
2019-11-05 08:19:50 -0600

    convert to hal::v2 traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
 //! Crate to interface stepper motor drivers
 
 #![deny(missing_docs)]
-#![deny(warnings)]
+//#![deny(warnings)]
 #![no_std]
-
-extern crate embedded_hal;
 
 pub mod ic;
 
 use core::marker::PhantomData;
 
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::v2::OutputPin;
 
 enum State {
     HIGH,
@@ -32,7 +30,7 @@ const STEPS: [[State; 4]; 4] = [
     [State::HIGH, State::LOW, State::LOW, State::HIGH],
 ];
 
-fn digital_write(state: &State, pin: &mut OutputPin) {
+fn digital_write<ERROR>(state: &State, pin: &mut dyn OutputPin<Error = ERROR>) -> Result<(), ERROR>{
     match state {
         &State::HIGH => pin.set_high(),
         &State::LOW => pin.set_low(),
@@ -40,12 +38,12 @@ fn digital_write(state: &State, pin: &mut OutputPin) {
 }
 
 /// A stepper motor driver
-pub struct Stepper<IN1, IN2, IN3, IN4, IC>
+pub struct Stepper<IN1, IN2, IN3, IN4, IC, ERROR>
 where
-    IN1: OutputPin,
-    IN2: OutputPin,
-    IN3: OutputPin,
-    IN4: OutputPin,
+    IN1: OutputPin<Error = ERROR>,
+    IN2: OutputPin<Error = ERROR>,
+    IN3: OutputPin<Error = ERROR>,
+    IN4: OutputPin<Error = ERROR>,
 {
     index: usize,
     /// clockwise or counterclockwise rotation
@@ -57,12 +55,12 @@ where
     _ic: PhantomData<IC>,
 }
 
-impl<IN1, IN2, IN3, IN4, IC> Stepper<IN1, IN2, IN3, IN4, IC>
+impl<IN1, IN2, IN3, IN4, IC, ERROR> Stepper<IN1, IN2, IN3, IN4, IC, ERROR>
 where
-    IN1: OutputPin,
-    IN2: OutputPin,
-    IN3: OutputPin,
-    IN4: OutputPin,
+    IN1: OutputPin<Error = ERROR>,
+    IN2: OutputPin<Error = ERROR>,
+    IN3: OutputPin<Error = ERROR>,
+    IN4: OutputPin<Error = ERROR>,
 {
     /// Change the stepper direction
     pub fn direction(&mut self, direction: Direction) {
@@ -70,11 +68,11 @@ where
     }
 
     /// Make the stepper move one step
-    pub fn step(&mut self) -> &mut Self {
-        digital_write(&STEPS[self.index][0], &mut self.in1);
-        digital_write(&STEPS[self.index][1], &mut self.in2);
-        digital_write(&STEPS[self.index][2], &mut self.in3);
-        digital_write(&STEPS[self.index][3], &mut self.in4);
+    pub fn step(&mut self) -> Result<&mut Self, ERROR> {
+        digital_write(&STEPS[self.index][0], &mut self.in1)?;
+        digital_write(&STEPS[self.index][1], &mut self.in2)?;
+        digital_write(&STEPS[self.index][2], &mut self.in3)?;
+        digital_write(&STEPS[self.index][3], &mut self.in4)?;
 
         self.index = match self.direction {
             Direction::CW => {
@@ -92,25 +90,25 @@ where
                 }
             }
         };
-        self
+        Ok(self)
     }
 
     /// Disable stepper
-    pub fn disable(&mut self) -> &mut Self {
-        self.in1.set_low();
-        self.in2.set_low();
-        self.in3.set_low();
-        self.in4.set_low();
-        self
+    pub fn disable(&mut self) -> Result<&mut Self, ERROR> {
+        self.in1.set_low()?;
+        self.in2.set_low()?;
+        self.in3.set_low()?;
+        self.in4.set_low()?;
+        Ok(self)
     }
 }
 
-impl<IN1, IN2, IN3, IN4> Stepper<IN1, IN2, IN3, IN4, ic::ULN2003>
+impl<IN1, IN2, IN3, IN4, ERROR> Stepper<IN1, IN2, IN3, IN4, ic::ULN2003, ERROR>
 where
-    IN1: OutputPin,
-    IN2: OutputPin,
-    IN3: OutputPin,
-    IN4: OutputPin,
+    IN1: OutputPin<Error = ERROR>,
+    IN2: OutputPin<Error = ERROR>,
+    IN3: OutputPin<Error = ERROR>,
+    IN4: OutputPin<Error = ERROR>,
 {
     /// Creates a new `Stepper`
     pub fn uln2003(direction: Direction, in1: IN1, in2: IN2, in3: IN3, in4: IN4) -> Self {
